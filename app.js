4362775f8d43dfd5807cfda3eac1e24ef31dfb31
Tony Jimenez <ingtonyjimenez@gmail.com>
2025-03-08 18:10:04 -0600

    Se agregan los métodos de selección de nombre aleatorio, e impresión de pantalla

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+let amigos = [];
+let inputAmigo = document.getElementById("amigo");
+let ulListaAmigos = document.getElementById("listaAmigos");
+let addButton = document.querySelector(".button-add");
+let ulResultado = document.querySelector("#resultado");
+
+
+
+
+function agregarAmigo(){
+        let nuevoAmigo = amigos.push(inputAmigo.value);
+        ulListaAmigos.innerHTML  += `<li>${inputAmigo.value}</li>`;
+};
+
+function sortearAmigo(){
+    let random = Math.floor((Math.random() * amigos.length));
+    let amigoSecreto = amigos[random];
+    ulResultado.innerHTML = `<li> El amigo secreto es: ${amigoSecreto}</li>`;
+  
+}
+
+
